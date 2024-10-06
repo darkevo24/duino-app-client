39235e128ed353ba126c0ebf8850187d06894309
darkevo24 <timotius_albert@yahoo.com>
2024-10-06 08:39:23 +0700

    up

--- src/components/servers/custom-server.vue
+++ src/components/servers/custom-server.vue
@@ -15,7 +15,7 @@
         </v-chip>
       </template>
     </v-text-field>
-    <v-btn @click="addServer" class="ml-2" :disabled="!serverData">
+    <v-btn @click="addServer" class="ml-2">
       <v-icon left>mdi mdi-plus</v-icon>
       Add Server
     </v-btn>
@@ -35,10 +35,11 @@
 </template>
 
 <script>
+
 export default {
   data() {
     return {
-      url: 'https://compile.duino.app', // Set initial server address
+      url: 'https://compile.duino.app',
       serverData: null,
       err: '',
       success: false,
@@ -46,45 +47,35 @@ export default {
   },
   computed: {
     address() {
-      // Ensure proper protocol handling
-      const trimmedUrl = this.url.trim().replace(/\/$/, '').replace(/^https?:\/\//, '');
-      const protocol = trimmedUrl.includes('http://') ? 'http://' : 'https://';
-      return `${protocol}${trimmedUrl}`;
+      const protocol = this.url.includes('http://') ? 'http://' : 'https://';
+      return `${protocol}${this.url.trim().replace(/\/$/, '').replace(/^https?:\/\//, '')}`;
     },
   },
   methods: {
     async checkUrl() {
       if (!this.url) return;
-
       const { Server } = this.$FeathersVuex.api;
       this.serverData = null;
       this.err = '';
-
       const serv = { address: this.address };
-
-      // Check if the server already exists
-      const existingServers = await Server.find({ query: serv });
-      if (existingServers.length) {
+      if ((await Server.find({ query: serv })).length) {
         this.err = 'Server already exists.';
         return;
       }
-
-      // Using Promises to handle server request without try/catch
-      this.$compiler.serverReq('info/server', null, serv)
-        .then((data) => {
-          this.serverData = data;
-          if (!this.serverData) {
-            this.err = 'Unable to connect to the server.';
-          }
-        })
-        .catch(() => {
-          this.err = 'Invalid server address.';
-        });
+      try {
+        this.serverData = await this.$compiler.serverReq('info/server', null, serv);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+        this.err = 'Invalid server address.';
+      }
+      if (!this.serverData) {
+        this.err = 'Unable to connect to the server.';
+      }
     },
     async addServer() {
       await this.checkUrl();
       if (!this.serverData) return;
-
       const { Server } = this.$FeathersVuex.api;
       const server = new Server({
         ...this.serverData,
@@ -92,10 +83,8 @@ export default {
         address: this.address,
         valid: true,
       });
-
-      // Save the new server
       await server.save();
-      this.url = 'https://compile.duino.app'; // Reset to initial server address
+      this.url = '';
       this.serverData = null;
       this.success = true;
       this.$store.commit('setCurrentServer', server.uuid);
